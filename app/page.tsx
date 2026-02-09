fa11d3951b5a4c6d647542e0d42d450cf1f0c59e
Azmil Mohammed <azmilmohammed@Azmils-MacBook-Air.local>
2026-02-09 15:21:46 +0530

    implemented code overview and trendingcoins with fetcher-supense

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import CoinOverview from '@/components/home/CoinOverview'
+import TrendingCoins from '@/components/home/TrendingCoins'
+import {
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from '@/components/home/fallback'
 
-const Page = () => {
-    return <div>
+const Page = async () => {
+  return (
+    <main className='main-container'>
+      <section className='home-grid'>
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-    </div>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className='w-full mt-7 space-y-4'>
+        <Suspense fallback={<CategoriesFallback />}></Suspense>
+      </section>
+    </main>
+  )
 }
+
 export default Page
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatCurrency = (amount: number, currency: string = 'inr') => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: currency,
+  }).format(amount)
 }
